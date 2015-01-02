d74d228369f76bce0e7345b2f9fd2fdf9ab670eb
theTrueCaptian <my_pink_han@yahoo.com>
2015-01-02 16:32:33 -0500

    Coloring appear

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ exports.index = function(req, res){
  
 	new YQL.exec('select * from html where url="http://www.pewforum.org/2011/01/27/table-muslim-population-by-country/" and xpath="//table"', function(response) {
 	 
-		if(response.query.results!=undefined){
+		if(response!=undefined && response.query !=undefined && response.query.results!=undefined){
 			//response consists of JSON of Muslim population
 			res.render('index', { title: 'Hijab Map', data: response.query.results.table });
+		}else{
+			res.render('index', { title: 'Hijab Map', data: 'Conn err'});
 		}
 	});
 	
